6c79bdf7b9daeaf07e8783d818d32885eb623d29
John Ogletree <131814728+Koam03@users.noreply.github.com>
2024-07-30 22:38:43 -0400

    components/mets.html scripts/fetch_meta.js

--- scripts/fetch_meta.js
+++
@@ -1,9 +0,0 @@
-function loadMeta() {
-    fetch('components/meta.html')
-      .then(response => response.text())
-      .then(data => {
-        const MetaElement = document.getElementById('meta-container');
-        MetaElement.innerHTML = data;
-      });
-  }
-  window.onload = loadMeta;
