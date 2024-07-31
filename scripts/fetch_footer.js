9435667a6c1ab8b6adad145406352cc806cf46e5
John Ogletree <131814728+Koam03@users.noreply.github.com>
2024-07-30 22:40:20 -0400

    index.html components/footer.html scripts/fetch_footer.js

--- scripts/fetch_footer.js
+++
@@ -1,9 +0,0 @@
-function loadFooter() {
-    fetch('components/footer.html')
-      .then(response => response.text())
-      .then(data => {
-        const footerElement = document.getElementById('footer-container');
-        footerElement.innerHTML = data;
-      });
-  }
-  window.onload = loadFooter;
